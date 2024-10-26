a44ff39d3b373141b0e0ed50ea058032c9a35d42
Tanmay7387 <103013867+Tanmay7387@users.noreply.github.com>
2024-10-26 18:06:31 +0100

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "key_name" {
+  description = "The name of the key pair registered in AWS"
+  type        = string
+}
+
+variable "private_key_path" {
+  description = "Path to the private key file"
+  type        = string
+}
